fb82d5900ed9f29217aef0bf6629cb8391c14882
Yashomathi <yashomathibatkeeri@gmail.com>
2025-05-08 18:27:34 -0700

    oq_put

--- proj/shufflenet_cfu/src/software_cfu.cc
+++ proj/shufflenet_cfu/src/software_cfu.cc
@@ -237,7 +237,7 @@ void oq_put(struct OutputQueue* oq, uint32_t word) {
   oq->w = (oq->w + 1) % EBRAM_DEPTH_WORDS;
   static int dbg_ctr = 0;
   if (dbg_ctr++ == 0) {
-    printf("oqput: 0x%08lx\n", oq->data[oq->w]);
+    printf("oqput shuff: 0x%08lx\n", oq->data[oq->w]);
   }
 }
 
